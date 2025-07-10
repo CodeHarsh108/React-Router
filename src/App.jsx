97c667176ac50b27d0ecef8ab45c7a33238697a8
Harsh Patil <itsharshhh6@gmail.com>
2025-07-10 08:43:25 +0530

    TailwindCSS Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -9,6 +9,13 @@ function App() {
   return (
     <div>
       <Router>
+        <nav>
+          <ul>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/about'}>About</Link></li>
+            <li><Link to={'/contact'}>Contact</Link></li>
+          </ul>
+        </nav>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
